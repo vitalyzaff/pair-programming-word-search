c5fc90cca549f008b9510460168e1d4e661156ba
Vitaly Zaff <vitalyzavgorodnev@gmail.com>
2021-05-05 20:37:35 +0000

    Pair programming with Sacha

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -31,8 +31,25 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is not present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'R', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'A', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'N', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'K', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK');
+
+    assert.isTrue(result);
+  });  
 });
+
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-}
+  }
+  const vertical = transpose(letters);
+  if (vertical) {
+    const verticalJoin = vertical.map(ls => ls.join(''));
+    for (let l of verticalJoin) {
+      if (l.includes(word)) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+  
+const transpose = function(matrix) {
+  let matrixReloaded = [];
+    
+  matrix.map((rowY, indY) => {
+    rowY.map((columnX, indX) => {
+      if (indY === 0) {
+        matrixReloaded.push([]);
+      }
+      matrixReloaded[indX].push(matrix[indY][indX]);
+    });
+  });
+  return matrixReloaded;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
